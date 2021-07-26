5afd28dd4a0f7da0cad8409ae1ec49f10e94225f
Tuyishimire Ange Victorien <68068538+Tuyishimire-lab@users.noreply.github.com>
2021-07-26 21:50:07 +0200

    Update convert_numbers.c

--- convert_numbers.c
+++ convert_numbers.c
@@ -69,15 +69,12 @@ unsigned int convert_di(va_list args, buffer_t *output,
 	if (ZERO_FLAG == 0 && d < 0)
 		ret += _memcpy(output, &neg, 1);
 	/* Handle plus flag when zero flag is not active */
-	
 	if (ZERO_FLAG == 0 && (PLUS_FLAG == 1 && d >= 0))
 		ret += _memcpy(output, &plus, 1);
-        
-	if (!(d == 0 && prec == 0))
+        if (!(d == 0 && prec == 0))
 		ret += convert_sbase(output, d, "0123456789",
 				flags, 0, prec);
-        
-	ret += print_neg_width(output, ret, flags, wid);
+        ret += print_neg_width(output, ret, flags, wid);
 
 	return (ret);
 }
